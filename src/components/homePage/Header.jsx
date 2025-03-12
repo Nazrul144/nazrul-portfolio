f0e065d78f5769c450288fcce94b31eb2a30a87c
Nazrul144 <nazrulislam.cse28@gmail.com>
2025-03-12 20:22:14 +0600

    Home page done

--- src/components/homePage/Header.jsx
+++ src/components/homePage/Header.jsx
@@ -8,13 +8,11 @@ import { useCallback } from "react";
 import Particles from "react-particles";
 import { loadSlim } from "tsparticles-slim";
 import { FaFacebook, FaGithub, FaLinkedinIn, FaTwitter, FaYoutube } from "react-icons/fa";
-import { BsLinkedin } from "react-icons/bs";
+import { TiSocialLinkedinCircular } from "react-icons/ti";
 
 const Header = () => {
-
   const [showFullText, setShowFullText] = useState(false);
 
- 
   const particlesInit = useCallback(async (engine) => {
     console.log(engine);
 
@@ -149,39 +147,59 @@ const Header = () => {
               Hi, I’m Nazrul Islam, a passionate Frontend Web Developer from Bangladesh. I specialize in building seamless,
               user-friendly web applications using modern technologies like HTML, CSS, Tailwind CSS, JavaScript, React, Next.js,
               Firebase, Express, and MongoDB. With a strong focus on performance, usability, and design, I craft web experiences
-              that enhance user engagement. {
-                showFullText && (
-                  <span> I thrive in collaborative environments, aligning technical solutions with project
-                  goals to deliver innovative and efficient applications. I’m always open to new opportunities and collaborations in
-                  web development. Feel free to connect with me at nazrulislam.cse28@gmail.com to discuss potential projects.</span>
-                )
-              }
+              that enhance user engagement.{" "}
+              {showFullText && (
+                <span>
+                  {" "}
+                  I thrive in collaborative environments, aligning technical solutions with project goals to deliver innovative
+                  and efficient applications. I’m always open to new opportunities and collaborations in web development. Feel
+                  free to connect with me at nazrulislam.cse28@gmail.com to discuss potential projects.
+                </span>
+              )}
             </p>
-            <button onClick={()=> setShowFullText(!showFullText)} className="border-[1px] text-white rounded-full text-xs border-red-600 px-4 py-2 bg-transparent font-bold  cursor-pointer mt-4 hover:bg-pink-600 hover:shadow-[0_0_20px_5px_rgba(236,72,153,0.6)] transition-all duration-300 ease-in-out">
-              {
-               showFullText ? "Read Less" : "Read More"
-              }
+            <button
+              onClick={() => setShowFullText(!showFullText)}
+              className="border-[1px] text-white rounded-full text-xs border-red-600 px-4 py-2 bg-transparent font-bold  cursor-pointer mt-4 hover:bg-pink-600 hover:shadow-[0_0_20px_5px_rgba(236,72,153,0.6)] transition-all duration-300 ease-in-out"
+            >
+              {showFullText ? "Read Less" : "Read More"}
             </button>
 
             <div className="flex items-center gap-12">
               <div>
-              <button className="border-[1px] text-white rounded-full text-xs border-red-600 px-4 py-2 bg-transparent font-bold flex gap-2 items-center hover:bg-pink-600 hover:shadow-[0_0_20px_5px_rgba(236,72,153,0.6)] transition-all duration-300 ease-in-out mt-12">
-              <a className="flex items-center gap-2" href="/cover/cv.pdf" download="cv">
-                Download CV <FileDownload  />
-              </a>
-            </button>
+                <button className="border-[1px] text-white rounded-full text-xs border-red-600 px-4 py-2 bg-transparent font-bold flex gap-2 items-center hover:bg-pink-600 hover:shadow-[0_0_20px_5px_rgba(236,72,153,0.6)] transition-all duration-300 ease-in-out mt-12">
+                  <a className="flex items-center gap-2" href="/cover/cv.pdf" download="cv">
+                    Download CV <FileDownload />
+                  </a>
+                </button>
               </div>
               <div className="mt-12 flex gap-4 items-center">
-             <a href="https://github.com/Nazrul144" target="_blank"><FaGithub className="text-2xl hover:bg-pink-600 rounded-full hover:shadow-[0_0_20px_5px_rgba(236,72,153,0.6)] transition-all duration-300 ease-in-out hover:text-3xl"/></a>
+                <a href="https://github.com/Nazrul144" target="_blank">
+                  <FaGithub
+                    className="text-2xl rounded-full text-white transition-all duration-300 ease-in-out hover:shadow-[0_0_15px_4px_rgba(236,72,153,0.6)] hover:scale-110 hover:text-3xl"/>
+                </a>
 
-             <a href="https://www.linkedin.com/in/nazrul-islam-a6080730b" target="_blank"><FaLinkedinIn className="text-2xl hover:bg-pink-600  hover:shadow-[0_0_20px_5px_rgba(236,72,153,0.6)] transition-all duration-300 ease-in-out"/></a>
+                <a href="https://www.linkedin.com/in/nazrul-islam-a6080730b" target="_blank">
+                  <TiSocialLinkedinCircular
+                    className="text-3xl rounded-full text-white transition-all duration-300 ease-in-out hover:shadow-[0_0_15px_4px_rgba(236,72,153,0.6)] hover:scale-110 hover:text-3xl"/>
+                </a>
 
-              <a href="https://x.com/Nazrul211002144" target="_blank"><FaTwitter className="text-2xl hover:bg-pink-600 rounded-full hover:shadow-[0_0_20px_5px_rgba(236,72,153,0.6)] transition-all duration-300 ease-in-out hover:text-3xl"/></a>
+                <a href="https://x.com/Nazrul211002144" target="_blank">
+                  <FaTwitter
+                    className="text-2xl rounded-full text-white transition-all duration-300 ease-in-out hover:shadow-[0_0_15px_4px_rgba(236,72,153,0.6)] hover:scale-110 hover:text-3xl"
+                  />
+                </a>
 
-              <a href="https://www.facebook.com/profile.php?id=100007196595674" target="_blank"><FaFacebook className="text-2xl hover:bg-pink-600 rounded-full hover:shadow-[0_0_20px_5px_rgba(236,72,153,0.6)] transition-all duration-300 ease-in-out hover:text-3xl"/></a>
+                <a href="https://www.facebook.com/profile.php?id=100007196595674" target="_blank">
+                  <FaFacebook
+                    className="text-2xl rounded-full text-white transition-all duration-300 ease-in-out hover:shadow-[0_0_15px_4px_rgba(236,72,153,0.6)] hover:scale-110 hover:text-3xl"
+                  />
+                </a>
 
-              <a href="https://www.youtube.com/@GKWORLD-kw1zv" target="_blank"><FaYoutube className="text-2xl hover:bg-pink-600 rounded-full hover:shadow-[0_0_20px_5px_rgba(236,72,153,0.6)] transition-all duration-300 ease-in-out hover:text-3xl"/>
-              </a>
+                <a href="https://www.youtube.com/@GKWORLD-kw1zv" target="_blank">
+                  <FaYoutube
+                    className="text-2xl rounded-full text-white transition-all duration-300 ease-in-out hover:shadow-[0_0_15px_4px_rgba(236,72,153,0.6)] hover:scale-110 hover:text-3xl"
+                  />
+                </a>
               </div>
             </div>
           </div>
