c3c3ef8167373b32d12930726935f18968c515da
Nazrul144 <nazrulislam.cse28@gmail.com>
2025-03-15 18:17:12 +0600

    -Swiper added

--- src/components/homePage/Projects.jsx
+++ src/components/homePage/Projects.jsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import React from "react";
 
 import React, { useRef, useState } from 'react';
 // Import Swiper React components
@@ -7,43 +6,46 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 
 // Import Swiper styles
 import 'swiper/css';
-import 'swiper/css/effect-fade';
-import 'swiper/css/navigation';
+import 'swiper/css/effect-flip';
 import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 
-import './styles.css';
 
 // import required modules
-import { EffectFade, Navigation, Pagination } from 'swiper/modules';
+import { EffectFlip, Pagination, Navigation } from 'swiper/modules';
+
 
 const Projects = () => {
   return (
-    <div className="px-4 lg:px-20">
+    <div className="px-4 lg:px-20 relative">
       <div className="flex flex-col lg:flex-row">
         <div className="lg:w-[50%] border-2 border-red-400">Content1</div>
-        <div className="lg:w-[50%]">
+
+        <div className="lg:w-[50%] relative">
           <Swiper
-            spaceBetween={30}
-            effect={"fade"}
+            effect={"flip"}
+            grabCursor={true}
+            pagination={true}
             navigation={true}
-            pagination={{
-              clickable: true,
-            }}
-            modules={[EffectFade, Navigation, Pagination]}
+            modules={[EffectFlip, Pagination, Navigation]}
             className="mySwiper"
           >
-            <SwiperSlide>
-              <Image src={'projects_image/1.png'} alt="Cover1" width={600} height={500}/>
+            <SwiperSlide >
+                <Image className="lg:ml-28 lg:mt-12" src={'/projects_image/1.jpg'} alt="Project1" width={500} height={500}/>
             </SwiperSlide>
             <SwiperSlide>
-            <Image src={'projects_image/2.png'} alt="Cover1" width={600} height={500}/>
+            <Image className="lg:ml-28 lg:mt-12" src={'/projects_image/1.jpg'} alt="Project1" width={500} height={500}/>
             </SwiperSlide>
             <SwiperSlide>
-            <Image src={'projects_image/3.png'} alt="Cover1" width={600} height={500}/>
+            <Image className="lg:ml-28 lg:mt-12" src={'/projects_image/1.jpg'} alt="Project1" width={500} height={500}/>
             </SwiperSlide>
             <SwiperSlide>
-            <Image src={'projects_image/4.png'} alt="Cover1" width={600} height={500}/>
+            <Image className="lg:ml-28 lg:mt-12" src={'/projects_image/1.jpg'} alt="Project1" width={500} height={500}/>
             </SwiperSlide>
+            <SwiperSlide>
+            <Image className="lg:ml-28 lg:mt-12" src={'/projects_image/1.jpg'} alt="Project1" width={500} height={500}/>
+            </SwiperSlide>
+           
           </Swiper>
         </div>
       </div>
