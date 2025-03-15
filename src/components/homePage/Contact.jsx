e49e92ce5869f9dcc529de37f697adca5ae25401
Nazrul144 <nazrulislam.cse28@gmail.com>
2025-03-15 15:31:00 +0600

    Contact functionality added

--- src/components/homePage/Contact.jsx
+++ src/components/homePage/Contact.jsx
@@ -1,13 +1,17 @@
 import React from "react";
-import { FaPhone, FaPhoneAlt, FaPhoneSquareAlt } from "react-icons/fa";
+import { FaPhoneAlt } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+import { FaLocationDot } from "react-icons/fa6";
+import { Button } from "@mui/material";
+import SendIcon from '@mui/icons-material/Send';
 
 const Contact = () => {
   return (
     <div className="px-4 lg:px-20">
-      <div className="lg:flex">
+      <div className="lg:flex gap-12">
         <div className="lg:max-w-[50%] border-2 border-red-500">
           <h1 className=" text-2xl lg:text-3xl font-extrabold mb-4">Let's Work Together</h1>
-          <p className="text-justify">
+          <p className="text-justify text-sm">
             I'm always open to exciting projects! Whether you're looking to create a website, build a web application, or just
             need some advice, I'd love to collaborate. Let's bring your ideas to life together. Feel free to reach out!
           </p>
@@ -23,7 +27,7 @@ const Contact = () => {
 
           <div className="flex items-center gap-4 mt-6">
             <div>
-              <FaPhoneAlt className="text-4xl text-red-600 p-2 bg-gray-300 rounded-sm " />
+              <MdEmail className="text-4xl text-red-600 p-2 bg-gray-300 rounded-sm " />
             </div>
             <div>
               <p className="text-sm font-semibold text-red-600">Email</p>
@@ -33,15 +37,67 @@ const Contact = () => {
 
           <div className="flex items-center gap-4 mt-6">
             <div>
-              <FaPhoneAlt className="text-4xl text-red-600 p-2 bg-gray-300 rounded-sm " />
+              <FaLocationDot className="text-4xl text-red-600 p-2 bg-gray-300 rounded-sm " />
             </div>
             <div>
-              <p className="text-sm font-semibold text-red-500">Phone</p>
-              <p className="text-sm text-gray-200">+88 01758752528</p>
+              <p className="text-sm font-semibold text-red-500">Address</p>
+              <p className="text-sm text-gray-200">Mirpur 10, Dhaka, Bangladesh</p>
             </div>
           </div>
         </div>
-        <div className="lg:max-w-[50%] border-2 border-green-500">Contact Form</div>
+
+        <div className="lg:max-w-[50%] border-[1px] border-white shadow-4xl  rounded-lg">
+          <h1 className="text-center font-extrabold text-white text-3xl mt-12">
+            <span className="text-red-600">Contact</span> Me
+          </h1>
+          {/*Contact Form*/}
+          <div>
+            {/* Form*/}
+            <div className="bg-transparent  my-12 rounded-lg lg:px-16 ">
+              <form>
+                <div className="grid grid-cols-2 gap-5 w-full">
+                  <input
+                    className="px-4 py-2 rounded-lg outline-none border-[1px] border-white shadow-4xl text-sm"
+                    name="user_name"
+                    type="text"
+                    placeholder="Full Name"
+                  />
+
+                  <input
+                    className="px-4 py-2 rounded-lg border-[1px] border-white shadow-4xl text-sm"
+                    name="from_email"
+                    type="text"
+                    placeholder="Email Address"
+                    required
+                  />
+                  <input
+                    className="px-4 py-2 rounded-lg border-[1px] border-white shadow-4xl text-sm"
+                    name="from_email"
+                    type="text"
+                    placeholder="Phone Number"
+                    required
+                  />
+                  <input
+                    className="px-4 py-2 rounded-lg border-[1px] border-white shadow-4xl text-sm "
+                    name="from_email"
+                    type="text"
+                    placeholder="Email Subject"
+                  />
+                </div>
+                <textarea
+                  className="w-full rounded-lg mt-6 text-stone-500 pl-2 pt-2 h-44 border-[1px] border-white shadow-4xl text-sm"
+                  required
+                  name="message"
+                  id=""
+                  placeholder="Message"
+                ></textarea>
+                <Button type="submit" variant="contained" endIcon={<SendIcon />}>
+                  Send Message
+                </Button>
+              </form>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
