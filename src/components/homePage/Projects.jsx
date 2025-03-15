972b5a01b63677344c85c31ab48ed35584996656
Nazrul144 <nazrulislam.cse28@gmail.com>
2025-03-15 21:30:43 +0600

    Dynamic data added for swiper

--- src/components/homePage/Projects.jsx
+++ src/components/homePage/Projects.jsx
@@ -9,26 +9,35 @@ import 'swiper/css';
 import 'swiper/css/effect-flip';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
-
-
 // import required modules
 import { EffectFlip, Pagination, Navigation } from 'swiper/modules';
 
 
+
 const Projects = () => {
+
+  const [activeSlide, setActiveSlide] = useState(1)
+
+  const handleSlideChange = (swiper)=>{
+    setActiveSlide(swiper.realIndex + 1);
+  }
+  
   return (
     <div className="px-4 lg:px-20 relative">
       <div className="flex flex-col lg:flex-row">
-        <div className="lg:w-[50%] border-2 border-red-400">Content1</div>
+        <div className="lg:w-[50%] border-2 border-red-400">Content{activeSlide}</div>
 
         <div className="lg:w-[50%] relative">
           <Swiper
+            initialSlide={0}
             effect={"flip"}
             grabCursor={true}
-            pagination={true}
+            pagination={{clickable: true}}
             navigation={true}
             modules={[EffectFlip, Pagination, Navigation]}
             className="mySwiper"
+            onSlideChange={handleSlideChange}
+            loop={false}
           >
             <SwiperSlide >
                 <Image className="lg:ml-28 lg:mt-12" src={'/projects_image/1.jpg'} alt="Project1" width={500} height={500}/>
