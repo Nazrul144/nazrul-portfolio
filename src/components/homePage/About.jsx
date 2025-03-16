9435d13f832c68ca96a10aab7a92c34d8f8d46b3
Nazrul144 <nazrulislam.cse28@gmail.com>
2025-03-16 23:29:35 +0600

    Framer motion added for skills section

--- src/components/homePage/About.jsx
+++ src/components/homePage/About.jsx
@@ -21,25 +21,61 @@ const skills = [
   "/skills/MySql.png",
 ];
 
-
 const About = () => {
   return (
     <div className="px-4 lg:px-20">
       <div className="lg:flex gap-6 ">
-        <div className="lg:w-[40%] ">
+        <motion.div
+          initial={{ x: -100, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ delay: 0.2, x: { type: "spring", stiffness: 60 }, opacity: { duration: 1 }, ease: "easeIn", duration: 1 }}
+          className="lg:w-[40%] "
+        >
           <div className="fireBorder lg:mt-20">
-            <Image className="brightness-75" src={"/about/cover11.png"} alt="About_image" width={1000} height={1000}  layout="intrinsic" />
+            <Image
+              className="brightness-75"
+              src={"/about/cover11.png"}
+              alt="About_image"
+              width={1000}
+              height={1000}
+              layout="intrinsic"
+            />
           </div>
           <h1 className=" ml-24 lg:ml-28 mt-4 font-bold animate-pulse bg-gradient-to-r from-blue-500 to-purple-500 bg-clip-text text-transparent">
-          MERN Stack Developer</h1>
-        </div>
-        <div className="lg:w-[60%]">
-          <h1 className="text-xl lg:text-3xl font-extrabold mb-2 mt-4 lg:mt-0">
-            <span className="text-red-600">About</span> Me
+            MERN Stack Developer
           </h1>
-          <p className="text-justify text-gray-100 text-sm">
-          I'm Nazru Islam, a MERN Stack Web Developer skilled in React, Next.js, JavaScript, Firebase, MongoDB, Tailwind CSS, and Material UI. I specialize in building scalable web applications and enjoy creating user-friendly, impactful solutions. Passionate about continuous learning, collaboration, and contributing to meaningful projects.
-          </p>
+        </motion.div>
+        <div className="lg:w-[60%]">
+          <motion.h1
+            initial={{ y: -50, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{
+              delay: 0.2,
+              x: { type: "spring", stiffness: 70 },
+              opacity: { duration: 1 },
+              ease: "easeIn",
+              duration: 1,
+            }}
+            className="text-xl lg:text-3xl font-extrabold mb-2 mt-4 lg:mt-0"
+          >
+            <span className="text-red-600">About</span> Me
+          </motion.h1>
+          <motion.p
+            initial={{ x: 100, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{
+              delay: 0.3,
+              x: { type: "spring", stiffness: 60 },
+              opacity: { duration: 2 },
+              ease: "easeIn",
+              duration: 1,
+            }}
+            className="text-justify text-gray-100 text-sm"
+          >
+            I'm Nazru Islam, a MERN Stack Web Developer skilled in React, Next.js, JavaScript, Firebase, MongoDB, Tailwind CSS,
+            and Material UI. I specialize in building scalable web applications and enjoy creating user-friendly, impactful
+            solutions. Passionate about continuous learning, collaboration, and contributing to meaningful projects.
+          </motion.p>
           {/*Tabs*/}
           <div className="mt-4 mb-4">
             <Tabs
@@ -57,7 +93,18 @@ const About = () => {
                   key: "1",
                   children: (
                     <div className="text-white">
-                      <div className="grid grid-cols-5 gap-4 px-2 py-1 lg:px-8 lg:py-4">
+                      <motion.div
+                        initial={{ x: -100, opacity: 0 }}
+                        animate={{ x: 0, opacity: 1 }}
+                        transition={{
+                          delay: 0.2,
+                          x: { type: "spring", stiffness: 60 },
+                          opacity: { duration: 1 },
+                          ease: "easeIn",
+                          duration: 1,
+                        }}
+                        className="grid grid-cols-5 gap-4 px-2 py-1 lg:px-8 lg:py-4"
+                      >
                         {skills?.map((skill, index) => (
                           <Card
                             key={index}
@@ -77,7 +124,7 @@ const About = () => {
                             </motion.div>
                           </Card>
                         ))}
-                      </div>
+                      </motion.div>
                     </div>
                   ),
                 },
@@ -90,7 +137,9 @@ const About = () => {
                   key: "3",
                   children: (
                     <div>
-                      <h1 className="font-bold text-xl text-white px-4">My <span className="text-red-600">Experience</span></h1>
+                      <h1 className="font-bold text-xl text-white px-4">
+                        My <span className="text-red-600">Experience</span>
+                      </h1>
                       <p className="text-gray-100 text-justify mt-4 text-xs mb-8 px-4">
                         I have gained valuable experience in various fields, including video editing, Photoshop, and web
                         development. My expertise in teaching English from 2018 to 2024 has strengthened my communication skills.
@@ -147,7 +196,9 @@ const About = () => {
                   key: "2",
                   children: (
                     <div>
-                      <h1 className="font-bold text-xl text-white px-4">My <span className="text-red-600">Education</span></h1>
+                      <h1 className="font-bold text-xl text-white px-4">
+                        My <span className="text-red-600">Education</span>
+                      </h1>
                       <p className="text-gray-100 text-justify mt-4 text-xs mb-8 px-4">
                         I hold a B.Sc. in Computer Science and Engineering from Green University of Bangladesh, with a strong
                         foundation. My education has equipped me with a blend of technical skills, critical thinking, and
